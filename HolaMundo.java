6721d6cc20bf5574cf08bf461d93ed065bdf715a
Cha0smagick <alekos20@gmail.com>
2019-05-08 10:47:46 -0500

    HolaMundo.java
    programa de holamundo en java
     by: Cha0smagick
    twitter: https://twitter.com/FreakBizarro
    twitch: https://www.twitch.tv/cha0smagick

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+//programa de holamundo en java
+// by: Freakbizarro / Cha0smagick
+// twitter: https://twitter.com/FreakBizarro
+//twitch: https://www.twitch.tv/cha0smagick
+
+public class holamundo 
+	{
+	public static void main(String[] args) 
+		{		
+		System.out.println("Hola Mundo");
+		}
+	}
